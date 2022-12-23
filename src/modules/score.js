43b14b5aff8da949c4e9d94a4d4564cb60e6f345
Israel CHIZUNGU <chizunguisrael@gmail.com>
2022-12-23 17:23:20 +0200

    Clear code

--- src/modules/score.js
+++ src/modules/score.js
@@ -1,6 +1,6 @@
 const scores = [{ id: 1, name: 'will', score: 144 }, { id: 2, name: 'gloria', score: 98 }, { id: 3, name: 'john', score: 102 }];
 
-const addscore = score => scores.push(score);
+const addscore = (score) => scores.push(score);
 
 const loadScore = () => scores;
 
