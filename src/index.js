b6b6c081bc47877e0dce6243bcb01cd4f7428cfd
Israel CHIZUNGU <chizunguisrael@gmail.com>
2022-12-23 17:01:11 +0200

    Use of ES6

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import './index.css';
+import { loadScore, addscore, showScore } from './modules/score';
+
+const DOMContainer = document.querySelector('.data-source');
+window.onload= ()=>{
+  // Fetch scores
+  const scores = loadScore();
+  scores.forEach(score=>{
+    showScore(score, DOMContainer);
+  });
+};
+
+document.getElementById(`refresh-btn`).onclick = ()=>{
+  // clear current items
+  DOMContainer.innerHTML = ``;
+  // Fetch scores
+  const scores = loadScore();
+  scores.forEach(score=>{
+    showScore(score, DOMContainer);
+  });
+};
+
+document.getElementById('form').onsubmit = (e)=>{
+  e.preventDefault();
+  const scores = loadScore();
+  const id = scores.length > 0 ? scores[scores.length-1].id + 1 : 1;
+  const name = document.getElementById('name').value;
+  const score = document.getElementById('score').value;
+  const obj = {id:id, name:name, score: score};
+  addscore(obj);
+  showScore(obj, DOMContainer);
+}
