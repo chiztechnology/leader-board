2d79064ca74889957693e6fa0294f4b12e66a6b8
Israel CHIZUNGU <chizunguisrael@gmail.com>
2022-12-23 17:04:00 +0200

    Clear code

--- src/index.js
+++ src/index.js
@@ -2,31 +2,31 @@ import './index.css';
 import { loadScore, addscore, showScore } from './modules/score';
 
 const DOMContainer = document.querySelector('.data-source');
-window.onload= ()=>{
+window.onload = () => {
   // Fetch scores
   const scores = loadScore();
-  scores.forEach(score=>{
+  scores.forEach(score => {
     showScore(score, DOMContainer);
   });
 };
 
-document.getElementById(`refresh-btn`).onclick = ()=>{
+document.getElementById('refresh-btn').onclick = () => {
   // clear current items
   DOMContainer.innerHTML = ``;
   // Fetch scores
   const scores = loadScore();
-  scores.forEach(score=>{
+  scores.forEach(score => {
     showScore(score, DOMContainer);
   });
 };
 
-document.getElementById('form').onsubmit = (e)=>{
+document.getElementById('form').onsubmit = (e) => {
   e.preventDefault();
   const scores = loadScore();
-  const id = scores.length > 0 ? scores[scores.length-1].id + 1 : 1;
+  const id = scores.length > 0 ? scores[scores.length - 1].id + 1 : 1;
   const name = document.getElementById('name').value;
   const score = document.getElementById('score').value;
-  const obj = {id:id, name:name, score: score};
+  const obj = { id: id, name: name, score: score };
   addscore(obj);
   showScore(obj, DOMContainer);
 }
